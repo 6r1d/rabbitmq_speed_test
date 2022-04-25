4a4c65f0b6d1fa17e1b437b30027f244e738bbfe
Grid <vic.6r1d@gmail.com>
2022-04-25 12:30:18 +0300

    Detaches the paragraph generator

---
+++ generator/hs_gen/gen.py
@@ -0,0 +1,73 @@
+"""
+This file contains a class to generate random paragraphs
+using Hunspell dicts.
+
+Confused? Think Lorem Ipsum without Latin. Or with some surprise Latin.
+"""
+
+from string import ascii_letters
+from random import choice, randint
+
+class ParagraphGenerator:
+    """A random paragraph generator class that uses the
+       Hunspell dictionary."""
+
+    def __init__(
+        self,
+        dict_path='/usr/share/hunspell/en_GB-large.dic',
+        min_sentence_length=4,
+        max_sentence_length=16,
+        min_sentences=5,
+        max_sentences=10
+    ):
+        self.dict_path = dict_path
+        self.normal, self.capitalized = self.get_dictionary()
+        self.min_sentence_length = min_sentence_length
+        self.max_sentence_length = max_sentence_length
+        self.min_sentences = min_sentences
+        self.max_sentences = max_sentences
+
+    def get_dictionary(self):
+        dict_words = []
+        with open(self.dict_path, 'r') as dict_file:
+            dict_words = list(
+                filter(
+                    lambda word: not bool(set(word) - set(ascii_letters)),
+                    [line.strip().split('/')[0] for line in dict_file.readlines()]
+                )
+            )
+        return (
+            list(filter(lambda word: word[0].islower(), dict_words)),
+            list(filter(lambda word: word[0].isupper(), dict_words))
+        )
+
+    def generate_sentence(self):
+        """
+        Returns:
+
+            str: a single sentence
+        """
+        words = [
+            choice(self.capitalized),
+            *[
+                choice(self.normal)
+                for _ in range(randint(
+                    self.min_sentence_length - 1,
+                    self.max_sentence_length - 1
+                ))
+            ]
+        ]
+        sign = choice(['.', '?', '!'])
+        return ' '.join(words) + sign
+
+    def generate_paragraph(self):
+        """
+        Returns:
+
+            str: a single paragraph
+        """
+        sentences = [
+            self.generate_sentence()
+            for _ in range(randint(self.min_sentences, self.max_sentences))
+        ]
+        return ' '.join(sentences)
--- generator/main.py
+++ generator/main.py
@@ -3,40 +3,21 @@
 import pika
 from os import environ
 from time import sleep
-from string import ascii_letters
-from random import choice, randint
+from hs_gen.gen import ParagraphGenerator
 
 START_WAIT_S = float(environ.get('START_WAIT_S', 5))
 DELAY_S = float(environ.get('MSG_DELAY_S', 1))
-DICT_PATH = '/usr/share/hunspell/en_GB-large.dic'
-
-def get_dictionary():
-    dict_words = []
-    with open(DICT_PATH, 'r') as dict_file:
-        dict_words = list(
-            filter(
-                lambda word: not bool(set(word) - set(ascii_letters)),
-                [line.strip().split('/')[0] for line in dict_file.readlines()]
-            )
-        )
-    return (
-        list(filter(lambda word: word[0].islower(), dict_words)),
-        list(filter(lambda word: word[0].isupper(), dict_words))
-    )
-
-def generate_sentence(normal, capitalized):
-    words = []
-    words.append( choice(capitalized) )
-    words.extend( [choice(normal) for word in range(randint(3, 15))] )
-    return ' '.join(words) + '.'
-
-def generate_paragraph(normal, capitalized):
-    sentences = [generate_sentence(normal, capitalized) for _ in range(randint(5, 10))]
-    return ' '.join(sentences)
 
 def main():
-    normal, capitalized = get_dictionary()
-
+    # Init the paragraph generator while
+    # passing Docker-compose environment variables
+    gen = ParagraphGenerator(
+        min_sentence_length=int(environ.get('MIN_SENTENCE_LENGTH'), 10),
+        max_sentence_length=int(environ.get('MAX_SENTENCE_LENGTH'), 10),
+        min_sentences=int(environ.get('MIN_SENTENCES'), 10),
+        max_sentences=int(environ.get('MAX_SENTENCES'), 10)
+    )
+    # Establish the RabbitMQ connection
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(
             'rabbitmq_inst',
@@ -51,7 +32,7 @@ def main():
     print(' [*] Starting the message generator. To exit press CTRL+C\n')
 
     while True:
-        paragraph = generate_paragraph(normal, capitalized)
+        paragraph = gen.generate_paragraph()
         channel.basic_publish(
             exchange='', routing_key='hello', body=paragraph
         )
